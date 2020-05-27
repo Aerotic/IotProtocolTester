a2cef9920fdeff1626682bac79314d3ae6ef56b9
Aerotic <neuhutao@gmail.com>
2020-05-27 18:38:31 +0800

    更改格式及读取协议成功

--- IotProtocolTester.py
+++ IotProtocolTester.py
@@ -9,87 +9,131 @@ class IotProtocolTester:
     # 格式为：
     # 0号：项目名称 String
     # 1号：bit长度 int
-    # 2号：数据类型 String int为整型数 float为单精度浮点 double为双精度浮点 str为字符串
+    # 2号: 起始地址
+    # 3号：数据类型 String int为整型数 float为单精度浮点 double为双精度浮点 str为字符串
     #       uint8_t     <->       B     标准长度1
     #       int8_t      <->       b     标准长度1
-    #       char[1]     <->       c     标准长度1
+    #       char[1]     <->       c     标准长度1  实际就是长度为1的字符串
     #       uint16_t    <->       H     标准长度2
     #       int16_t     <->       h     标准长度2
-    #       uint8_t     <->       B
-    #       uint8_t     <->       B
-    #       uint8_t     <->       B
-    #       uint8_t     <->       B
-    #       uint8_t     <->       B
-    #       uint8_t     <->       B
-
+    #       uint32_t    <->       I     标准长度4
+    #       int32_t     <->       i     标准长度4
+    #       uint64_t    <->       Q     标准长度8
+    #       int64_t     <->       q     标准长度8
+    #       float       <->       f     标准长度4
+    #       double      <->       d     标准长度8
+    #       char[]      <->       s     标准长度
+    #       uint8_t     <->       B     标准长度
     pItems = list()
-    
+
+    # struct.pack unpack所需的格式解析字符串
+    pFmtStr = '!'
+
     # 格式为：
     # 0号：项目名称 String
     # 1号：bit起始
     # 2号：bit终止
     pItemBitFields = list()
+    
 
-    bitCnt = 0 # 这是一个统计各项所占位数的计数器，若bitCnt大于pLen*8, 则说明协议制定的有问题
+    
+    byteCnt = 0 # 这是一个统计各项所占字节数的计数器
     pLen = 0 # 这是从json读出得协议长度（以字节计数）
 
     def __init__(self, path,port):
         self.getJsonData(path)
         self.getProtocolInfoFromJson()
-        if self.isProtocolLenValid() == False:
-            print("FATAL! Protocol Length is less than sum of item length")
-            exit()
-        self.getItemBitFields()
+        # if self.isProtocolLenValid() == False:
+        #     print("FATAL! Protocol Length is less than sum of item length")
+        #     exit()
+        # self.getItemBitFields()
         pass
 
-    def getJsonData(self,path): 
-        jsonText = open(path).read() # 打开json文件并读取所有内容到 jsonText
-        self.jsonData = json.loads(jsonText) # 将Json项目名和值存入JsonData
+    def getJsonData(self,path):
+        # 打开json文件并读取所有内容到 jsonText
+        jsonText = open(path).read()
+        # 将Json项目名和值存入JsonData
+        self.jsonData = json.loads(jsonText) 
 
     def getProtocolInfoFromJson(self):
+        self.byteCnt = 0
         for i in self.jsonData:
-            if i == "len_byte":
-                self.pLen = self.jsonData[i]
-                continue
-            item = i
-            bits,type = (str((self.jsonData[item])).split("+"))
-            bits = int(bits)
-            if  type == "int":
-                self.pItems.append((item,bits,type,int(0)))
-            elif type == "float":
-                if bits != 32 :
-                    self.pItems.append((item,bits,"TYPE LENGTH ERROR"))
-                else:
-                    self.pItems.append((item,bits,type))
-            elif type == "double": # 一般Python中的Float就是双精度的
-                if bits != 64 :
-                    self.pItems.append((item,bits,"TYPE LENGTH ERROR"))
-                else:
-                    self.pItems.append((item,bits,type))
-            elif type == "str":
-                self.pItems.append((item,bits,type))
-            else:
-                self.pItems.append((item,bits,"TYPE ERROR"))
-                
-    def isProtocolLenValid(self):
-        """
-        判断协议总长度是否大于各个分项长度和
-        """
-        # 统计每个项目的bit长度和
-        for item in self.pItems:
-            self.bitCnt = self.bitCnt + item[1] #int(item[1])
-        # 若bitCnt*8大于pLen, 则说明协议制定的有问题
-        if (self.bitCnt > self.pLen * 8):
-            return False
-        else: 
-            return True
 
-    def getItemBitFields(self):
-        bitPtr = 0
-        for i in self.pItems:
-            start = bitPtr
-            bitPtr = bitPtr + i[1] # 加上项目长度
-            self.pItemBitFields.append((i[0],start,bitPtr - 1))
+            
+            # 名称
+            item = i 
+
+            # 更新格式字符串
+            self.pFmtStr = self.pFmtStr + str(self.jsonData[item]) 
+
+            # 获取长度和起始地址，更新字节计数器
+            if len(str(self.jsonData[item])) == 1:
+                byte_len = 1;
+                type = str(self.jsonData[item])[0]
+            else:
+                byte_len = int(str(self.jsonData[item])[0])
+                type = str(self.jsonData[item])[1]
+                
+            startAddr = self.byteCnt
+            self.byteCnt = self.byteCnt + byte_len
+
+            # 获取类型
+            typeStr = 'Not Initialized'
+            #       uint8_t     <->       B     标准长度1
+            #       int8_t      <->       b     标准长度1
+            #       char[1]     <->       c     标准长度1  实际就是长度为1的字符串
+            #       uint16_t    <->       H     标准长度2
+            #       int16_t     <->       h     标准长度2
+            #       uint32_t    <->       I     标准长度4
+            #       int32_t     <->       i     标准长度4
+            #       uint64_t    <->       Q     标准长度8
+            #       int64_t     <->       q     标准长度8
+            #       float       <->       f     标准长度4
+            #       double      <->       d     标准长度8
+            #       char[]      <->       s     标准长度
+            if  type == "B":
+                typeStr = 'uint8_t'
+            elif type == "b":
+                typeStr = 'int8_t'
+            elif type == "c":
+                typeStr = 'char[1]'
+            elif type == "H":
+                typeStr = 'uint16_t'
+                 
+            elif type == "h":
+                typeStr = 'int16_t'
+                 
+            elif type == "I":
+                typeStr = 'uint32_t'
+                 
+            elif type == "i":
+                typeStr = 'int32_t'
+                 
+            elif type == "Q":
+                typeStr = 'uint64_t'
+                 
+            elif type == "q":
+                typeStr = 'int64_t'
+                 
+            elif type == "b":
+                typeStr = 'int8_t'
+                 
+            elif type == "f":
+                typeStr = 'float'
+                 
+            elif type == "d":
+                typeStr = 'double'
+                 
+            elif type == "s":
+                typeStr = 'char[]'
+                 
+            else:
+                typeStr = "TYPE ERROR"
+            # 更新项目名称，起始地址，长度，类型
+            self.pItems.append((item,startAddr,byte_len,typeStr))
+        
+        # 更新协议长度
+        self.pLen = self.byteCnt
 
     def updateHostIp(self):
         """
@@ -104,17 +148,42 @@ class IotProtocolTester:
             s.close()
         return self.hostIp
 
-# iot = IotProtocolTester("demo.json",10086)
 
-data = struct.pack('!4Bf', 10, 35, 12, 13, 2.333)
-print(str(data))
-arr=(1,2,3,4)
-arr,fl = struct.unpack('!4Bf',data)
-print(arr,"\n",fl)
-# print(iot.pLen)
-# for i in iot.pItemBitFields:
-#     print(i)
+    def unpack(self,dat):
+        tmpTuple = tuple()
+        
+
+    def funcname(self, parameter_list):
+        pass
+iot = IotProtocolTester("demo.json",10086)
+print(iot.pFmtStr)
+for i in iot.pItems:
+    print(">>>",i)
+    pass
+
+# 名，类型，长度，起始地址
+
+
+# tri = tuple()
+data = struct.pack('!B4B5s', 5, 10, 35, 12, 13, bytes('12'.encode('utf-8')))
+# print(str(data))
+# arr=(1,2,3,4)
+# tri = struct.unpack('!B4B5s',data)
+# l = list(tri[1:4])
+# print(data[4])
+# print(data[5])
+# print(data[6])
+# print(data[7])
+# # data[7] = '\0'
+# print(data[8])
+# print(l)
+# print(len('4B'))
+# # print(str(l, encoding = "utf-8"))
+# # for i in tri:
+    
 #     pass
+# print(iot.pLen)
+
     # # 打开json文件并读取所有内容到json_text
     # json_text= open("t.json").read()
 
